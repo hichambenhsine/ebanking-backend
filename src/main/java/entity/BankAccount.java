023b6849678e57937f1efc394e7131cbe5f5dd2d
Hicham <benhsinehm@gmail.com>
2026-05-01 23:35:14 +0100

    feat(account): add abstract BankAccount entity

---
+++ src/main/java/entity/BankAccount.java
@@ -0,0 +1,24 @@
+package entity;
+
+import enums.AccountStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE",length = 4)
+@Data @NoArgsConstructor @AllArgsConstructor
+public abstract class BankAccount {
+    @Id
+    private String id;
+    private double balance;
+    private Date createdAt;
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+}
