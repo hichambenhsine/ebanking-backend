134683e5cd8cce7e4c7455571b5bacb7b1921c13
Hicham <benhsinehm@gmail.com>
2026-05-01 23:44:52 +0100

    fix(customer): add OneToMany relationship with BankAccount

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -1,12 +1,12 @@
 package entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
@@ -15,4 +15,7 @@ public class Customer {
     private Long id;
     private String name;
     private String email;
+    @OneToMany(mappedBy = "customer")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<BankAccount> bankAccounts;
 }
