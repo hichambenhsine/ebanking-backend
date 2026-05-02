3547e51af91682564237c598a64fb54f5424a62c
Hicham <benhsinehm@gmail.com>
2026-05-02 18:42:57 +0100

    feat(repository): add query methods to AccountOperationRepository
    - Add findByBankAccountId(String accountId)
    - Add findByBankAccountIdOrderByOperationDateDesc(String accountId, Pageable pageable)

--- src/main/java/org/glsid/ebankingbackend/repository/AccountOperationRepository.java
+++ src/main/java/org/glsid/ebankingbackend/repository/AccountOperationRepository.java
@@ -2,6 +2,12 @@ package org.glsid.ebankingbackend.repository;
 
 import org.glsid.ebankingbackend.entity.AccountOperation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface AccountOperationRepository extends JpaRepository<AccountOperation, Long> {
+    List<AccountOperation> findByBankAccountId(String accountId);
+    Page<AccountOperation> findByBankAccountIdOrderByOperationDateDesc(String accountId, Pageable pageable);
 }
